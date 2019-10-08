8c0b2d2deeee45c27dc332b700cf3abaedafa489
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-10-08 08:53:07 +0100

    Implements play method for FizzBuzz

--- src/com/caitlin/Main.java
+++ src/com/caitlin/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        play(9);
+        play(10);
+        play(30);
+    }
+
+    public static void play(Integer number) {
+        if (number % 15 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        }
     }
 }
+
+
+// If number is divisble by 3 print fizz
+// If number is divisble by 5 print buzz
+// If number is divisble by 15 print fizzbuzz
