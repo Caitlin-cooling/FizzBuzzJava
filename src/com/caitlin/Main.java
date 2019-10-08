6142697dc02dd149e254fb41c011968602b24c59
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-10-08 08:56:52 +0100

    Refactors to use check15 method

--- src/com/caitlin/Main.java
+++ src/com/caitlin/Main.java
@@ -10,12 +10,22 @@ public class Main {
     }
 
     public static void play(Integer number) {
+        if (check15(number) == "Try again") {
+            if (number % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz");
+            }
+        } else {
+            System.out.println(check15(number));
+        }
+    }
+
+    public static String check15(Integer number) {
         if (number % 15 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (number % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (number % 5 == 0) {
-            System.out.println("Buzz");
+            return "FizzBuzz";
+        } else {
+            return "Try again";
         }
     }
 }
