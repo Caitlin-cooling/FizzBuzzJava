550b8c9b1fc83b76a176f182515f24d90b078196
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-10-09 08:44:58 +0100

    Refactors to use check methods

--- src/com/caitlin/Main.java
+++ src/com/caitlin/Main.java
@@ -1,36 +1,51 @@
 package com.caitlin;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        play(7);
         play(9);
         play(10);
         play(30);
     }
 
     public static void play(Integer number) {
-        if (check15(number) == "Try again") {
-            if (number % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (number % 5 == 0) {
-                System.out.println("Buzz");
-            }
+        if (check15(number)) {
+            System.out.println("FizzBuzz");
+        } else if (check3(number))  {
+             System.out.println("Fizz");
+         } else if (check5(number)) {
+            System.out.println("Buzz");
         } else {
-            System.out.println(check15(number));
+            System.out.println(number);
         }
     }
 
-    public static String check15(Integer number) {
+    public static Boolean check15(Integer number) {
         if (number % 15 == 0) {
-            return "FizzBuzz";
+            return true;
         } else {
-            return "Try again";
+            return false;
+        }
+    }
+
+    public static Boolean check3(Integer number) {
+        if (number % 3 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Boolean check5(Integer number) {
+        if(number % 5 == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
 
-
-// If number is divisble by 3 print fizz
-// If number is divisble by 5 print buzz
-// If number is divisble by 15 print fizzbuzz
