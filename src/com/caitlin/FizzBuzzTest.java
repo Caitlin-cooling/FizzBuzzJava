8f51ffc3be11dfe9c9d6dc05d2455fde4737a190
Caitlin-cooling <ccooling@deloitte.co.uk>
2019-10-11 09:06:31 +0100

    Refactors to use @Before

--- src/com/caitlin/FizzBuzzTest.java
+++ src/com/caitlin/FizzBuzzTest.java
@@ -1,22 +1,28 @@
 package com.caitlin;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+         fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void play_whenPassedNumber7_returns7() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.play(7);
         assertEquals("7", result);
     }
 
     @Test
     public void play_whenPassedNumber15_returnsFizzBuzz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.play(15);
         assertEquals("FizzBuzz", result);
     }
